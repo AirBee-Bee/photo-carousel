642c8945693b6d2cf1fa1e2b03151b55d95bd711
Joseph Dry <joe.dry6477@gmail.com>
2020-08-06 19:52:28 -0400

    Server responds to get request with appropriate photos

--- server/index.js
+++ server/index.js
@@ -1,5 +1,17 @@
 const express = require('express');
 const app = express();
+const db = require('./database/index.js');
 const port = 3000;
+
+app.get('/listing/:listingId', (req, res) => {
+  var listingId = req.params.listingId;
+  db.fetchPhotos(listingId, (err, data) => {
+    if (err) {
+      console.log(`Error fetching photos from db: ${err}`);
+    } else {
+      res.send(data);
+    }
+  });
+});
 
 app.listen(port, () => console.log(`Server is running on port ${port}`));
