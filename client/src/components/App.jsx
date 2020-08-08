2d60a8bce8527819901aa30c43786e5b09c7bc84
Joseph Dry <joe.dry6477@gmail.com>
2020-08-08 14:40:48 -0400

    Created App component that renders 5 images to the page

--- client/src/components/App.jsx
+++ client/src/components/App.jsx
@@ -1,9 +1,42 @@
 import React from 'react';
+import $ from 'jquery';
 
 class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      photos: []
+    };
+  }
+
+  componentDidMount() {
+    var listingId = 2; // This will eventually need to point to the listing id in the URL
+    $.ajax({
+      method: 'GET',
+      url: `/listing/${listingId}`,
+      success: photos => {
+        this.setState({
+          photos
+        });
+      },
+      error: err => {
+        console.log(err);
+      }
+    });
+  }
+
   render() {
+    var images = this.state.photos.slice(0, 5);
     return (
-      <h1>Test</h1>
+      <div>
+        {images.map((image, index) => (
+          <img
+            src={image.photo_url}
+            alt={image.photo_description}
+            key={index}
+          />
+        ))}
+      </div>
     );
   }
 }
