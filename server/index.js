e8f2d2c5be200e963184dd24b0b2d6ad99faa55e
Joseph Dry <joe.dry6477@gmail.com>
2020-08-12 14:12:29 -0400

    Page renders appropriately with url param

--- server/index.js
+++ server/index.js
@@ -1,11 +1,20 @@
 const express = require('express');
 const app = express();
 const db = require('./database/index.js');
+const path = require('path');
 const port = 3000;
 
-app.use(express.static(__dirname + '/../public/dist'));
 
 app.get('/listing/:listingId', (req, res) => {
+  if (req.params.listingId === 'bundle.js') {
+    res.sendFile('bundle.js', { root: path.join(__dirname, '../public/dist') });
+  } else {
+    res.sendFile('index.html', { root: path.join(__dirname, '../public/dist') });
+  }
+});
+
+
+app.get('/listing/:listingId/photos', (req, res) => {
   var listingId = req.params.listingId;
   db.fetchPhotos(listingId, (err, data) => {
     if (err) {
