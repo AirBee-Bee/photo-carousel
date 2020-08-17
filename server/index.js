acfaecc1a511b2c1c2a6212607a17ac699e83201
Joseph Dry <joe.dry6477@gmail.com>
2020-08-17 18:00:20 -0400

    Configured to work with proxy server

--- server/index.js
+++ server/index.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const db = require('./database/index.js');
 const path = require('path');
+const cors = require('cors');
 const port = 3000;
 
-app.use(express.json());
+app.use(cors());
 
 app.get('/listing/:listingId', (req, res) => {
   res.sendFile('index.html', { root: path.join(__dirname, '../public/dist') });
